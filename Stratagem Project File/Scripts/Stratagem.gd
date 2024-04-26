0aa4cfac54c8b255aad2b18d07731e47c0c85afe
StephenScarlett <sosiah13@gmail.com>
2024-04-26 13:53:36 -0400

    Stratagem script update
    Fixed the logic of how buttons appear and first attempt at adding colour to correct button presses.

--- Stratagem Project File/Scripts/Stratagem.gd
+++ Stratagem Project File/Scripts/Stratagem.gd
@@ -11,18 +11,41 @@ func _ready():
 	pass
 
 func _process(_delta):
-	if inputArray.size() > 0:
-		var last_element = inputArray[inputArray.size() - 1]
-		for i in range(resultArray.size()):
-			if last_element == resultArray[i]:
-				print("Last element of Array1 matches Array2 element at index:", i)
+	
+
+				
 	
 	
 	if inputArray == resultArray:
 		print("winner")
 		inputArray.clear()
 
-
+func check_for_successfull_input():
+	if inputArray.size() > 0:
+		var last_element = inputArray[inputArray.size() - 1]
+		for i in range(inputArray.size()):
+			var selectedStratagemPanel = $"../Arrows"
+			if last_element == resultArray[i]:
+				if last_element == "Left":
+					var arrow_node_name = "L" + str(i + 1)
+					var arrow_node = selectedStratagemPanel.get_node(arrow_node_name)
+					if arrow_node:
+						arrow_node.modulate = Color("0fca0d")
+				elif last_element == "Right":
+					var arrow_node_name = "L" + str(i + 1)
+					var arrow_node = selectedStratagemPanel.get_node(arrow_node_name)
+					if arrow_node:
+						arrow_node.modulate = Color("0fca0d")
+				elif last_element == "Up":
+					var arrow_node_name = "U" + str(i + 1)
+					var arrow_node = selectedStratagemPanel.get_node(arrow_node_name)
+					if arrow_node:
+						arrow_node.modulate = Color("0fca0d")
+				elif last_element == "Down":
+					var arrow_node_name = "U" + str(i + 1)
+					var arrow_node = selectedStratagemPanel.get_node(arrow_node_name)
+					if arrow_node:
+						arrow_node.modulate = Color("0fca0d")
 
 func stratagem_resupply():
 	$"../SelectedStratagemPanel/Stratagem Texture".texture = load(resu)
@@ -40,24 +63,26 @@ func reinforce_pressed():
 	
 	for i in range(resultArray.size()):
 		if resultArray[i] == "Left":
-			var arrow_node_name = "l" + str(i + 1)
+			var arrow_node_name = "L" + str(i + 1)
 			var arrow_node = selectedStratagemPanel.get_node(arrow_node_name)
 			if arrow_node:
 				arrow_node.visible = true
 		elif resultArray[i] == "Right":
-			var arrow_node_name = "r" + str(i + 1)
+			var arrow_node_name = "L" + str(i + 1)
 			var arrow_node = selectedStratagemPanel.get_node(arrow_node_name)
 			if arrow_node:
+				arrow_node.flip_h = true
 				arrow_node.visible = true
 		elif resultArray[i] == "Up":
-			var arrow_node_name = "u" + str(i + 1)
+			var arrow_node_name = "U" + str(i + 1)
 			var arrow_node = selectedStratagemPanel.get_node(arrow_node_name)
 			if arrow_node:
 				arrow_node.visible = true
 		elif resultArray[i] == "Down":
-			var arrow_node_name = "d" + str(i + 1)
+			var arrow_node_name = "U" + str(i + 1)
 			var arrow_node = selectedStratagemPanel.get_node(arrow_node_name)
 			if arrow_node:
+				arrow_node.flip_h = true
 				arrow_node.visible = true
 
 	
@@ -121,16 +146,17 @@ func machineGun_pressed():
 
 func _on_left_pressed():
 	inputArray.push_back("Left")
+	check_for_successfull_input()
 	
 
 func _on_right_pressed():
 	inputArray.push_back("Right")
-	print(inputArray)
+	check_for_successfull_input()
 
 func _on_up_pressed():
 	inputArray.push_back("Up")
-	print(inputArray)
+	check_for_successfull_input()
 
 func _on_down_pressed():
 	inputArray.push_back("Down")
-	print(inputArray)
+	check_for_successfull_input()
