91c880a7e781f1ac524998a5189c8d56fa477f23
StephenScarlett <sosiah13@gmail.com>
2024-04-29 17:23:37 -0400

    Stratagem script update
    Addressed the issue where correct input counted as wrong input

--- Stratagem Project File/Scripts/Stratagem.gd
+++ Stratagem Project File/Scripts/Stratagem.gd
@@ -11,21 +11,30 @@ func _ready():
 	pass
 
 func _process(_delta):
-	
-
-				
-	
-	
 	if inputArray == resultArray:
 		print("winner")
 		inputArray.clear()
 
+func shake(node):
+	node.position.x += 20
+	node.position.x -= 20
+
+func reset_children(node):
+	var children = node.get_children()
+	for child in children:
+		if child is Control:
+			child.visible = false
+			child.modulate = Color("ffffff")
+			child.flip_h = false
+			child.flip_v = false
+
+
 func check_for_successfull_input():
+	var selectedStratagemPanel = $"../Arrows"
 	if inputArray.size() > 0:
 		var last_element = inputArray[inputArray.size() - 1]
-		for i in range(inputArray.size()):
-			var selectedStratagemPanel = $"../Arrows"
-			if last_element == resultArray[i]:
+		for i in range(min(len(inputArray), len(resultArray))):
+			if inputArray[i] == resultArray[i]:
 				if last_element == "Left":
 					var arrow_node_name = "L" + str(i + 1)
 					var arrow_node = selectedStratagemPanel.get_node(arrow_node_name)
@@ -46,20 +55,26 @@ func check_for_successfull_input():
 					var arrow_node = selectedStratagemPanel.get_node(arrow_node_name)
 					if arrow_node:
 						arrow_node.modulate = Color("0fca0d")
+			else:
+				reset_children(selectedStratagemPanel)
+				print("restart")
+				print("input: ", inputArray)
+				print("result: ", resultArray)
+				inputArray.clear()
+					
+		
 
 func stratagem_resupply():
 	$"../SelectedStratagemPanel/Stratagem Texture".texture = load(resu)
 
 func reinforce_pressed():
+	var selectedStratagemPanel = $"../Arrows"
 	inputArray.clear()
-	
+	reset_children(selectedStratagemPanel)
 	#Load in selected stratagem texture
 	$"../Stratagem Texture".texture = load(rein)
 	#Set array of arrow directions
 	resultArray = ["Up","Down","Left","Right","Up"]
-	var selectedStratagemPanel = $"../Arrows"
-	for child in selectedStratagemPanel.get_children():
-		child.visible = false
 	
 	for i in range(resultArray.size()):
 		if resultArray[i] == "Left":
